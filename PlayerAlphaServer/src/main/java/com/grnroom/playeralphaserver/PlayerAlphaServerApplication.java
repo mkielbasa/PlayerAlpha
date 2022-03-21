0bc767940cf15bc2857055bc04768554ae24b05d
mkielbasa <102037190+mkielbasa@users.noreply.github.com>
2022-03-21 13:17:56 +0100

    Initial spring boot configuration

---
+++ PlayerAlphaServer/src/main/java/com/grnroom/playeralphaserver/PlayerAlphaServerApplication.java
@@ -0,0 +1,13 @@
+package com.grnroom.playeralphaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerAlphaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayerAlphaServerApplication.class, args);
+    }
+
+}
---
+++ PlayerAlphaServer/src/test/java/com/grnroom/playeralphaserver/PlayerAlphaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grnroom.playeralphaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerAlphaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
